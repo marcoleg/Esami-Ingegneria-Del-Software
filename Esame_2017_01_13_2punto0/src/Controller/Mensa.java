e7283de6b4b6d82f04b2bbcd5f6853718873f8c1
marco <marco@XPS-Legio.homenet.telecomitalia.it>
2017-12-29 10:51:51 +0100

    2.0

--- Esame_2017_01_13_2punto0/src/Controller/Mensa.java
+++ Esame_2017_01_13_2punto0/src/Controller/Mensa.java
@@ -1,7 +1,33 @@
 package Controller;
 
+import java.util.ArrayList;
+
+import Model.Persona;
+import Model.TesseraMensa;
+
 public class Mensa {
 
-	// metodi per l'aggiunta di persone e tessere
+	private ArrayList<Persona> listaPersone;
+	private ArrayList<TesseraMensa> listaTessereMensa;
+	private String nomeMensa;
 	
+	public Mensa(String nomeMensa) {
+		
+		this.listaPersone = new ArrayList<Persona>();
+		this.listaTessereMensa = new ArrayList<TesseraMensa>();
+		this.nomeMensa = nomeMensa;
+		
+	}
+	
+	public void aggiungiPersona(Persona persona) {
+		
+		this.listaPersone.add(persona);
+		
+	}
+	
+	public void aggiungiTessera(TesseraMensa tesseraMensa) {
+		
+		this.listaTessereMensa.add(tesseraMensa);
+		
+	}
 }
